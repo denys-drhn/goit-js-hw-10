ccebdf3dfbede1e5e83369da5b5cd95d941f09f8
Denys <denys.drhn@gmail.com>
2023-03-04 21:23:04 +0200

    update js
    code optimization

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -5,10 +5,10 @@ export function fetchCountries(name) {
 	return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
 		.then(response => {
 			if (!response.ok) {
-			return Notify.failure("Oops, there is no country with that name");
-    }
+				return Notify.failure("Oops, there is no country with that name");
+			}
 		return response.json();
-	}).catch(error => {
+		}).catch(error => {
 	console.log(error);
 });
 };
--- src/index.js
+++ src/index.js
@@ -11,76 +11,65 @@ const refs = {
 
 const DEBOUNCE_DELAY = 300;
 
-// function onSearchBoxInput();
-
 refs.textInput.addEventListener('input', debounce(onSearchBoxInput, 500));
 
 function onSearchBoxInput(event) {
 	let textInput = event.target.value.trim();
-	// console.log(textInput);
 
 	if (textInput.length > 0) {
 
 fetchCountries(`${textInput}`)
 	.then(data => {
-		if (!data) {			
-			refs.countryList.innerHTML = '';
-			refs.countryInfo.innerHTML = '';
-        return [];
-    }
-		// console.log(data);
-		if (data.length > 9) {
-			refs.countryList.innerHTML = '';
-			refs.countryInfo.innerHTML = '';
+// если не пришел промис
+		if (!data) {
+			clearMarkup()
+			return [];
+		}
+//если значений больше 10
+		if (data.length > 10) {
+			clearMarkup();
 			Notify.info("Too many matches found. Please enter a more specific name.");
 		}
-		return data;
-	}).then(listOfCountries => {
-		// console.log(listOfCountries);
-		if (listOfCountries.length >= 2 && listOfCountries.length < 9) {
-			// console.log('list of countries');
-			// return listOfCountries;
-			const markupCountriesList = makeMarkupCountriesList(listOfCountries);
-			refs.countryInfo.innerHTML = '';
-			refs.countryList.innerHTML= markupCountriesList;
+//если два и более значений
+		if (data.length >= 2 && data.length < 9) {
+			makeMarkupCountriesList(data);
 		}
-
-		if (listOfCountries.length === 1) {
-			// console.log(listOfCountries[0].name.official);
-			// console.log('one country');
-			const markupCountry = makeMarkupCountry(listOfCountries);
-			refs.countryList.innerHTML = markupCountry;
-			const markupCountryInfo = makeMarkupCountryInfo(listOfCountries);
-			refs.countryInfo.innerHTML = markupCountryInfo;
+//если одно значение
+		if (data.length === 1) {
+			makeMarkupCountryInfo(data);
 		}
-
-		return listOfCountries;
 	});
-
 	} else {
 		// console.log('не запрашиваем');
 	}
 };
 
-function makeMarkupCountryInfo(data) {
-	return `<p><b>Capital:</b> ${data[0].capital}</p>
-			<p><b>Population:</b> ${data[0].population}</p>
-			<p><b>Languages:</b> ${Object.values(data[0].languages).join(', ')}</p>`;
+function clearMarkup() {
+	refs.countryList.innerHTML = '';
+	refs.countryInfo.innerHTML = '';
 };
 
-function makeMarkupCountry(data) {
-	return `<li style="display: flex; align-items: center;">
-		<img src="${data[0].flags.svg}" alt="${data[0].flags.alt}" width="30" height="30"> 
-		<span style="font-size: 30px; font-weight: bold; padding-left: 10px;">${data[0].name.common}</span>
-	</li>`;
+function makeMarkupCountryInfo(data) {
+	const markup = `<div style="display: flex; align-items: center;">
+	<img src="${data[0].flags.svg}" alt="${data[0].flags.alt}" width="30" height="30">
+	<span style="font-size: 30px; font-weight: bold; padding-left: 10px;">${data[0].name.common}</span>
+	</div>
+	<p><b>Capital:</b> ${data[0].capital}</p>
+	<p><b>Population:</b> ${data[0].population}</p>
+	<p><b>Languages:</b> ${Object.values(data[0].languages).join(', ')}</p>`;
+	refs.countryList.innerHTML = '';
+	refs.countryInfo.innerHTML = markup;
 };
 
 function makeMarkupCountriesList(data) {
-	return data.map((country) => `<li style="display: flex; align-items: center;">
-		<img src="${country.flags.svg}" alt="${country.flags.alt}" width="30" height="30"> 
-		<span style="font-size: 20px; padding-left: 10px;">${country.name.common}</span>
-	</li>`).join("");	
+	const markup = data.map((country) => `<li style="display: flex; align-items: center;">
+	<img src="${country.flags.svg}" alt="${country.flags.alt}" width="30" height="30"> 
+	<span style="font-size: 20px; padding-left: 10px;">${country.name.common}</span>
+	</li>`).join("");
+	refs.countryInfo.innerHTML = '';
+	refs.countryList.innerHTML = markup;
 };
+
 
 // fetchCountries('ukr')
 // 	.then(data => {
